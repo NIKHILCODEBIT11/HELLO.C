dc98695144db24196832dad5b9d539bf52c8cd86
NIKHILCODEBIT11 <nikhilcodebit11@gmail.com>
2023-09-05 23:28:05 +0530

    added confirming feature.

--- program.c
+++ program.c
@@ -3,8 +3,10 @@ int main(){
     int a, b;
     printf("enter a");
     scanf("%d",&a);
+    printf("so a is %d \n",a);
     printf("enter b");
     scanf("%d",&b);
+    printf("so b is %d \n",b);
     int sum = a+b;
     printf("sum is %d",sum);
     return 0;
