2c8fa1ade67ed2ff3429c141002e05663c188edf
NIKHILCODEBIT11 <nikhilcodebit11@gmail.com>
2023-09-06 11:10:29 +0530

    working program of area of circle

--- program.c
+++ program.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 //area of square
 int main(){
-    int radius;
+    float radius;
     printf("enter radius");
-    scanf("%d",radius);
-    printf("so radius is %d \n",radius);
-    int area = 3.14*radius*radius;
-    printf("area of circle is %d",area);
+    scanf("%f",&radius);
+    printf("area of circle is %f", 3.14 * radius * radius);
     return 0;
 }
